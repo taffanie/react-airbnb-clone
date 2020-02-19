1ccc2d3e38e486d9709af09e03d4713d4255973b
Taffanie Ben <taffanie.ben@gmail.com>
2020-02-19 21:55:40 +0000

    added flat.js

--- src/components/hello.js
+++ src/components/hello.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Hello extends React.Component {
   render() {
-    return <div>Hello from hello.js</div>;
+    return <div>Hello {this.props.firstName} {this.props.lastName}!</div>;
   }
 }
 

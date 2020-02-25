fcbc507562d570871ae434d17a204aee6fff82d5
Taffanie Ben <taffanie.ben@gmail.com>
2020-02-25 22:01:49 +0000

    added 1 flat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Flat from './components/flat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const flat = {
+      "id": 145,
+      "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
+      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+      "price": 164,
+      "priceCurrency": "EUR",
+      "lat": 48.884211,
+      "lng": 2.346890
+    };
+
+    return (
+      <div>
+        <Flat flat={flat} />
+        <Flat flat={flat} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Hello from './components/hello';
-import * as serviceWorker from './serviceWorker';
+// import Hello from './components/hello';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Hello firstName="Boris" lastName="Paillard" />, document.getElementById('root'));
+// const root = <div>
+//   <Hello firstName="Boris" lastName="Paillard" />
+//   <Hello firstName="Romain" lastName="Paillard" />
+// </div>;
+
+// ReactDOM.render(root, document.getElementById('root'));
+
+// Inside the <App /> component (App.js), will use flat
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();

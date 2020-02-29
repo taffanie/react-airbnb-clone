f31aa556783048374bab1f3e2db9d9c61aeea0e0
Taffanie Ben <taffanie.ben@gmail.com>
2020-02-29 23:21:21 +0000

    completed

--- src/App.js
+++ src/App.js
@@ -2,23 +2,103 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Flat from './components/flat';
+import GoogleMapReact from 'google-map-react';
+import Marker from './components/marker';
 
 class App extends React.Component {
-  render() {
-    const flat = {
-      "id": 145,
-      "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-      "price": 164,
-      "priceCurrency": "EUR",
-      "lat": 48.884211,
-      "lng": 2.346890
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: [],
+      allFlats: [],
+      selectedFlat: null,
+      search: ""
     };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url) // AJAX
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data,
+          allFlats: data
+        });
+      })
+  }
+
+  selectFlat = (flat) => {
+    this.setState({
+      selectedFlat: flat
+    });
+  }
+
+  handleSearch = (event) => {
+    this.setState({
+      search: event.target.value,
+      flats: this.state.allFlats.filter((flat) => new RegExp(event.target.value, "i").exec(flat.name))
+    })
+  }
+
+  render() {
+    // const flat = {
+    //   "id": 145,
+    //   "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
+    //   "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+    //   "price": 164,
+    //   "priceCurrency": "EUR",
+    //   "lat": 48.884211,
+    //   "lng": 2.346890
+    // };
+
+    // const flats = [ flat, flat, flat, flat ];
+
+    const center = {
+      lat: 48.8566,
+      lng: 2.3522
+    }
+
+    if (this.state.selectedFlat) {
+      let center = { lat: this.state.selectedFlat.lat, lng: this.state.selectedFlat.lng }
+    }
+
 
     return (
-      <div>
-        <Flat flat={flat} />
-        <Flat flat={flat} />
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={this.state.search}
+            onChange={this.handleSearch} />
+        </div>
+        <div className="flats" >
+          {this.state.flats.map((flat) => {
+            return <Flat
+                    key={flat.name}
+                    flat={flat}
+                    selectFlat={this.selectFlat} />
+            })}
+        </div>
+        </div>
+         <div className="map">
+         <GoogleMapReact
+            center={center}
+            zoom={11}
+          >
+          {this.state.flats.map((flat) => {
+            return <Marker
+                    key={flat.name}
+                    lat={flat.lat}
+                    lng={flat.lng}
+                    text={flat.price}
+                    selected={flat === this.state.selectedFlat}
+                    />
+          })}
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
--- src/components/flat.js
+++ src/components/flat.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./flat.css";
 
 class Flat extends React.Component {
+  handleClick = () => {
+    // Call the parent method selectFlat
+    this.props.selectFlat(this.props.flat);
+  }
+
   render() {
 
     const title = this.props.flat.price + this.props.flat.priceCurrency + " - " + this.props.flat.name;
+
+    // this.props refers to variable const "flat" in App.js
 
     const style = {
       // Template literals ES6
@@ -12,7 +19,7 @@ class Flat extends React.Component {
     };
 
     return (
-      <div className="flat">
+      <div className="flat" onClick={this.handleClick}>
         <div className="flat-picture" style={style}></div>
         <div className="flat-title">
           {title}
